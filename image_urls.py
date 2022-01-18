7eee8cdcc87a69ba876053b92519e51c641c43fc
Mokshit Surana <mokshitsurana3110@gmail.com>
2022-01-19 01:19:00 +0530

    Gets the url for each of the image

---
+++ image_urls.py
@@ -0,0 +1,23 @@
+import googlemaps
+from urllib.parse import urlencode
+
+API_KEY = "YOUR_API_KEY"
+gmaps = googlemaps.Client(key=API_KEY)
+
+
+def get_image_urls(photo_references: list) -> list:
+    url = "https://maps.googleapis.com/maps/api/place/photo"
+    image_urls = []
+
+    for photo_ref in photo_references:
+        params = {
+            "photo_reference": photo_ref,
+            "key": API_KEY,
+            "maxwidth": 600,
+            "maxheight": 600
+        }
+        encoded_params = urlencode(params)
+        encoded_url = f"{url}?{encoded_params}"
+        image_urls.append(encoded_url)
+
+    return image_urls
