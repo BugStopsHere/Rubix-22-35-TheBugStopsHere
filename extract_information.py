ba1b34e86501a1f8e2045385a66b58160e9b3dbc
Mokshit Surana <mokshitsurana3110@gmail.com>
2022-01-19 01:10:11 +0530

    Extracts name, address, ratings and phone number of a given place

---
+++ extract_information.py
@@ -0,0 +1,35 @@
+import requests
+from urllib.parse import urlencode
+
+API_KEY = "AIzaSyB-BsREvkWKBFGRQWcmNtcES8k9WIArCnw"
+
+def extract_information(places_id: list) -> tuple[list, list, list, list]:
+    """
+    Returns names, address ratings and phone number(if given) of all the places of which ids are given in places_id
+    """
+    names = []
+    addresses = []
+    ratings = []
+    phone_nos = []
+    url_detail_endpoint = "https://maps.googleapis.com/maps/api/place/details/json"
+
+    for place_id in places_id:
+        params_detail = {
+            'place_id': place_id,
+            'fields': 'formatted_address,business_status,name,type,rating,formatted_phone_number,international_phone_number',
+            'key': API_KEY}
+
+        encoded_detail_params = urlencode(params_detail)
+        encoded_url_detail = f"{url_detail_endpoint}?{encoded_detail_params}"
+
+        r = requests.get(encoded_url_detail).json()
+        res = r['result']
+
+        names.append(res['name']) if "name" in res else None
+        ratings.append(res['rating']) if "rating" in res else None
+        addresses.append(res['formatted_address']
+                         ) if "formatted_address" in res else None
+        phone_nos.append(res['formatted_phone_number']
+                         ) if "formatted_phone_number" in res else None
+
+    return names, addresses, ratings, phone_nos
